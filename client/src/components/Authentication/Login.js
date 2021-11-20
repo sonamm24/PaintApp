d26dddf92ec7d6c780591c530451351c34d9ba55
Sonam <sonam.malhotra2496@gmail.com>
2021-11-20 21:05:47 +0000

    Updated password type

--- client/src/components/Authentication/Login.js
+++ client/src/components/Authentication/Login.js
@@ -25,7 +25,7 @@ export class Login extends React.Component {
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input type="text" name="password" placeholder="password" />
+            <input type="password" name="password" placeholder="password" />
           </div>
         </div>
       </div>
