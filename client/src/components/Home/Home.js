410af7cb5cee35d2de6fbaeca5d06f28020a0a43
Sonam <sonam.malhotra2496@gmail.com>
2021-11-27 01:48:58 +0000

    Added missing semicolon

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
 import './Home.css';
 
 export class Home extends React.Component {
 
   render() {
     return (
-    <div className="parallax">
+      <div className="parallax">
         <img
           alt=""
           className="image-1"
           src="https://www.color-meanings.com/wp-content/uploads/woman-painting-watercolor-floral-design-canvas.jpeg"
         />
-    </div>
+      </div>
     );
   }
 }
